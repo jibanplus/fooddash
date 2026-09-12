24347e93bca23ff1d4a88c56c4146c0490031394
jibanplus <100aditighosh@gmail.com>
2026-09-12 20:14:26 +0530

    Ignore TS and ESLint errors on build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // TypeScript error থাকলেও বিল্ড যেন না থামে
     ignoreBuildErrors: true,
   },
   eslint: {
-    // ESLint warning/error থাকলেও বিল্ড যেন পাস হয়
     ignoreDuringBuilds: true,
   },
 };
